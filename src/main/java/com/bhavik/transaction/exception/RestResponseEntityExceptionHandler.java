23a5ef231c05486af29730169b0eec96151ffa80
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-22 18:48:18 +0530

    handle all the exception

--- src/main/java/com/bhavik/transaction/exception/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/bhavik/transaction/exception/RestResponseEntityExceptionHandler.java
@@ -7,12 +7,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+public class RestResponseEntityExceptionHandler {
 
-	@ExceptionHandler({InsufficientFund.class})
+	@ExceptionHandler({ InsufficientFund.class })
 	public ResponseEntity<Object> handleInsufficientFundIn(InsufficientFund e) {
 		System.out.println("This is external Handler");
 		return new ResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
+	@ExceptionHandler({ RuntimeException.class })
+	public ResponseEntity<Object> handleAllException(RuntimeException e) {
+		return new ResponseEntity("Something went wrong", HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
--- src/main/java/com/bhavik/transaction/service/TransactionService.java
+++ src/main/java/com/bhavik/transaction/service/TransactionService.java
@@ -26,9 +26,9 @@ public class TransactionService {
 			throw new InsufficientFund("Insufficient Balance");
 		}
 		
-		
+		throw new RuntimeException();
 
-		return true;
+		//return true;
 	}
 
 }
