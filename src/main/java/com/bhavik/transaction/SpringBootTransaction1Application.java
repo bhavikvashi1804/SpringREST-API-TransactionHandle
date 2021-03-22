bd942865a54120cd4d25f3ac7a5f4a2f89f05171
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2021-03-22 18:01:48 +0530

    Initial Commit

---
+++ src/main/java/com/bhavik/transaction/SpringBootTransaction1Application.java
@@ -0,0 +1,13 @@
+package com.bhavik.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTransaction1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTransaction1Application.class, args);
+	}
+
+}
